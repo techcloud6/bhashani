d33c08516ae2426b096afafd283e9f3d6f15c2c1
surbhikothari03 <techcloud4@vsndirect.com>
2023-09-05 11:43:27 +0530

    structure update

--- bhashani/src/App.js
+++ bhashani/src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
+import Navabar from "./components/navbar/navbar";
+import Header from "./components/header/header";
 
 function App() {
   return (
     <div className="App">
-    
+    <Navabar />
+    <Header />
     </div>
   );
 }
---
+++ bhashani/src/components/navbar/navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function navbar() {
+  return (
+    <div>
+<h1>This is navabar</h1>
+    </div>
+  )
+}
