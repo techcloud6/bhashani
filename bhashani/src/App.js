ae46c6cb6fec22b74d70bc865b6be27c7cfd442e
Tarundoit07 <111743971+Tarundoit07@users.noreply.github.com>
2023-09-06 16:20:02 +0530

    css related issue

--- bhashani/src/App.js
+++ bhashani/src/App.js
@@ -3,6 +3,8 @@ import './App.scss';
 import Navabar from "./components/navbar/navbar";
 import Header from "./components/header/header";
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 
 
